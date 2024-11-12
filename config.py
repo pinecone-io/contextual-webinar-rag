4924965d42105df79c746ee02a6f652855b96f3e
Arjun Patel <arjun@pinecone.io>
2024-11-11 20:35:39 -0600

    cleanup

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 
+# Directories
 package_dir = Path(__file__).parent
 project_dir =   package_dir.parent
 data_dir = project_dir / "data"
-
 videos_dir = data_dir / "videos"
 
-
 # Pinecone variables that are helpful
-
 index_name = "test-vqa"
