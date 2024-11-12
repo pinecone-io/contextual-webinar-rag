ce42867f59fc0dbcc096c8fad6db8c162321f2e5
Arjun Patel <arjun@pinecone.io>
2024-11-12 16:02:41 -0600

    formatting with black

--- preprocessing/enrich_and_create_vectors.py
+++ preprocessing/enrich_and_create_vectors.py
@@ -1,10 +1,14 @@
-#this script take sthe output from preprocessing the video (transcript summary, frame caption pairs) and creates the high quality
+# this script take sthe output from preprocessing the video (transcript summary, frame caption pairs) and creates the high quality
 # contextual frame descriptions for our vector earch
 
-from claude_utils import create_contextual_frame_description, make_claude_transcript_summary
+from claude_utils import (
+    create_contextual_frame_description,
+    make_claude_transcript_summary,
+)
 from tqdm import tqdm
 
 import json
+
 with open("./data/all_videos_data.json", "r") as f:
     all_videos_data = json.load(f)
 
@@ -23,16 +27,17 @@ for video, data in all_videos_data.items():
 
     for i, pair in tqdm(enumerate(frame_caption_pairs)):
         contextual_frame_description = create_contextual_frame_description(
-            frame_caption_index = i, 
-            frame_caption_pairs=frame_caption_pairs, 
-            transcript_summary=transcript_summary)
-    # write out the updated frame caption pairs
+            frame_caption_index=i,
+            frame_caption_pairs=frame_caption_pairs,
+            transcript_summary=transcript_summary,
+        )
+        # write out the updated frame caption pairs
         new_pair = {
             "frame_path": pair["frame_path"],
             "words": pair["words"],
             "timestamp": pair["timestamp"],
             "transcript_summary": transcript_summary,
-            "contextual_frame_description": contextual_frame_description
+            "contextual_frame_description": contextual_frame_description,
         }
         finalized_data.append(new_pair)
 
--- preprocessing/preprocess_videos.py
+++ preprocessing/preprocess_videos.py
@@ -2,7 +2,7 @@ import ffmpeg
 from transformers import pipeline
 import os
 import torch
-import json 
+import json
 from config import videos_dir, data_dir
 
 # Important Globals
@@ -13,9 +13,12 @@ INTERVAL = 45
 # Step 1: Transcribe Video
 def transcribe_video(video_path):
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-tiny",device=device)
+    transcriber = pipeline(
+        "automatic-speech-recognition", model="openai/whisper-tiny", device=device
+    )
     transcription = transcriber(video_path, return_timestamps="word")
     return transcription
+
 
 # Step 2: Extract Frames and Pair with Dialogue
 def extract_frames(frames_output_path, video_path, interval):
@@ -30,19 +33,24 @@ def extract_frames(frames_output_path, video_path, interval):
     except FileExistsError:
         print(f"Folder already exists for videofile {video_path}")
         print("Please delete it to start fresh or ensure it is empty.")
-    
+
     # Use ffmpeg to extract frames
     (
-        ffmpeg
-        .input(video_path)
-        .filter('fps', fps=1/interval)
+        ffmpeg.input(video_path)
+        .filter("fps", fps=1 / interval)
         # this sets the output to be a frame every Interval seconds
-        .output(f'{frame_output}/frame_%04d.png')
+        .output(f"{frame_output}/frame_%04d.png")
         .run()
     )
-    
+
     # Collect the frame file paths
-    frame_files = sorted([os.path.join(frame_output, f) for f in os.listdir(frame_output) if f.endswith('.png')])
+    frame_files = sorted(
+        [
+            os.path.join(frame_output, f)
+            for f in os.listdir(frame_output)
+            if f.endswith(".png")
+        ]
+    )
     # create a dictionary of frame file paths and their corresponding timestamps according to interval and video length
 
     frames_and_intervals = []
@@ -50,16 +58,20 @@ def extract_frames(frames_output_path, video_path, interval):
     for i, frame in enumerate(frame_files):
         single_frame = {
             "frame_path": frame,
-            "timestamp": (interval_start, interval_start + (interval))
+            "timestamp": (interval_start, interval_start + (interval)),
         }
         frames_and_intervals.append(single_frame)
-        interval_start+=interval
-    
+        interval_start += interval
+
     # get video duration and make the last frame the same as the duration
-    video_duration = ffmpeg.probe(video_path)['format']['duration']
+    video_duration = ffmpeg.probe(video_path)["format"]["duration"]
     final_frame_start_time = frames_and_intervals[-1]["timestamp"][0]
-    frames_and_intervals[-1]["timestamp"] = (final_frame_start_time, float(video_duration))
+    frames_and_intervals[-1]["timestamp"] = (
+        final_frame_start_time,
+        float(video_duration),
+    )
     return frames_and_intervals
+
 
 def assign_words_to_frames(transcription, frames, interval=INTERVAL):
     # given transcription word chunks and frames that occur w.r.t interval, assign words to frames
@@ -70,30 +82,37 @@ def assign_words_to_frames(transcription, frames, interval=INTERVAL):
 
     for f in frames:
         frame_start, frame_end = f["timestamp"][0], f["timestamp"][1]
-        
+
         # filter for words that fall in frame
-        words_in_frame = list(filter(lambda w: w['timestamp'][0] > frame_start and w['timestamp'][1] < frame_end, transcription["chunks"]))
-        words = [w['text'] for w in words_in_frame]
-        words= "".join(words)
+        words_in_frame = list(
+            filter(
+                lambda w: w["timestamp"][0] > frame_start
+                and w["timestamp"][1] < frame_end,
+                transcription["chunks"],
+            )
+        )
+        words = [w["text"] for w in words_in_frame]
+        words = "".join(words)
         single_frame = {
             "frame_path": f["frame_path"],
             "words": words,
-            "timestamp": f["timestamp"]
+            "timestamp": f["timestamp"],
         }
         frames_and_words.append(single_frame)
     return frames_and_words
 
+
 def align_frames_with_dialogue(frames, transcription):
-    #Pinecone for t
+    # Pinecone for t
     dialogue_frames = []
     for frame, chunk in zip(frames, transcription["chunks"]):
-        dialogue_frames.append({
-            "frame": frame,
-            "dialogue": chunk["text"],
-            "metadata": {
-                "timestamp": chunk["timestamp"]
+        dialogue_frames.append(
+            {
+                "frame": frame,
+                "dialogue": chunk["text"],
+                "metadata": {"timestamp": chunk["timestamp"]},
             }
-        })
+        )
     return dialogue_frames
 
 
@@ -114,34 +133,37 @@ if __name__ == "__main__":
         os.mkdir(frames_and_words_dir)
         os.mkdir(frames_dir)
     except FileExistsError:
-        print("Folders already exist. Please delete them to start fresh or ensure they are empty.")
+        print(
+            "Folders already exist. Please delete them to start fresh or ensure they are empty."
+        )
 
     for video_path in video_files:
         transcription = transcribe_video(video_path)
 
         video_filename = os.path.splitext(os.path.basename(video_path))[0]
-        
+
         # Write transcription out as json
-        transcription_filename = os.path.join(transcriptions_dir, video_filename + "_transcription.json")
+        transcription_filename = os.path.join(
+            transcriptions_dir, video_filename + "_transcription.json"
+        )
         with open(transcription_filename, "w") as f:
             json.dump(transcription["text"], f)
-        
+
         frames = extract_frames(frames_dir, video_path, INTERVAL)
         frames_and_words = assign_words_to_frames(transcription, frames)
-        
-        frames_and_words_filename = os.path.join(frames_and_words_dir, video_filename + "_frames_and_words.json")
+
+        frames_and_words_filename = os.path.join(
+            frames_and_words_dir, video_filename + "_frames_and_words.json"
+        )
         with open(frames_and_words_filename, "w") as f:
             json.dump(frames_and_words, f)
-        
+
         all_videos_data[video_filename] = {
             "transcription": transcription_filename,
-            "frames_and_words": frames_and_words_filename
+            "frames_and_words": frames_and_words_filename,
         }
 
     # Optionally, write all_videos_data to a summary file
     all_videos_data_path = data_dir / "all_videos_data.json"
     with open(all_videos_data_path, "w") as f:
         json.dump(all_videos_data, f)
-
-
-    
