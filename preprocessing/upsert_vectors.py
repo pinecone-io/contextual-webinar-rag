7eb0aa2502162e937246798c1bd61927ce3e7392
Arjun Patel <arjun@pinecone.io>
2024-11-27 16:03:57 -0600

    more enviro issues

--- preprocessing/upsert_vectors.py
+++ preprocessing/upsert_vectors.py
@@ -14,8 +14,8 @@ import toml
 load_dotenv()
 
 boto3_session = boto3.session.Session(
-    aws_access_key_id=st.secrets["aws_access_key_id"],
-    aws_secret_access_key=st.secrets["aws_secret_access_key"]
+    aws_access_key_id=st.secrets["AWD_ACCESS_KEY_ID"],
+    aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
 )
 region_name = 'us-east-1'
 
